88426d5c5543f6fe40d4cfa6fba42c031a047346
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-07-31 23:25:25 +0700

    Update train.py

--- train.py
+++ train.py
@@ -269,7 +269,4 @@ def main():
     accelerator.end_training()
 
 if __name__ == "__main__":
-    print("Running")
-    print("GPU check:", torch.cuda.is_available())
-    print("Using device:", torch.cuda.get_device_name(torch.cuda.current_device()))
     main()
