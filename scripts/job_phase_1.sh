6ff79349f46810db656a7a0625ca020fdb8be534
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-08-07 09:46:47 +0700

    Update job_phase_1.sh

--- scripts/job_phase_1.sh
+++ scripts/job_phase_1.sh
@@ -7,13 +7,15 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 
+cd "$(dirname "$0")"
+
 module load slurm
 module load python39
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate dang_env
 
-rm -f ../outputs/FontDiffuser/fontdiffuser_training.log
+rm -f ../../outputs/FontDiffuser/fontdiffuser_training.log
 
 cd ~/data/fontdiffuser/FontDiffuser
 git pull origin main
