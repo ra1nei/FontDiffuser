7b47be9410f925a9be78296972e01b8625d18149
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-08-20 21:39:59 +0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,8 @@ from .dpm_solver.pipeline_dpm_solver import FontDiffuserDPMPipeline
 from .modules import (ContentEncoder,
                      StyleEncoder, 
                      UNet,
-                     SCR)
+                     SCR,
+                     SD3AdapterUNet)
 from .build import (build_unet, 
                    build_ddpm_scheduler, 
                    build_style_encoder, 
