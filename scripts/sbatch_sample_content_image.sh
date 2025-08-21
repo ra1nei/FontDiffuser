415c81ee2d94f6eb773b616d4cf9440fdfcb902b
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-08-21 12:45:47 +0700

    upload

---
+++ scripts/sbatch_sample_content_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -o logs/phase1_%j.out
+#SBATCH --time=120:00:00
+#SBATCH --gres=gpu:1
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+
+cd "$(dirname "$0")"
+
+module load slurm
+module load python39
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate dang_env
+
+rm -f ../../outputs/FontDiffuser/fontdiffuser_training.log
+
+cd ~/data/fontdiffuser/FontDiffuser
+git pull origin main
+
+bash scripts/sample_content_image.sh
