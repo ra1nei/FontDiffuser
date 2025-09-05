d65ed124f12eec9aeb27e687ded4d3f85a48f474
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-09-05 20:59:34 +0700

    Update font_dataset.py

--- dataset/font_dataset.py
+++ dataset/font_dataset.py
@@ -70,6 +70,7 @@ class FontDataset(Dataset):
         last_underscore_index = style_lang_part.rfind('_')
         style = style_lang_part[:last_underscore_index]
         lang = style_lang_part[last_underscore_index:]
+        print(f"Lang: {lang}")
         script = self.get_script(style + lang)
 
         # Load content image
