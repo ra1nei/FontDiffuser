e8aeba77eb217e9349ea4ed4f41a390fbef69ebf
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-09-09 00:10:29 +0700

    update

--- scripts/sbatch P1 UNET_SAME.sh
+++ scripts/sbatch P1 UNET_SAME.sh
@@ -10,6 +10,9 @@
 # Vào project
 cd ~/data/fontdiffuser/FontDiffuser || exit
 
+# Thêm WandB API key của bạn tại đây
+export WANDB_API_KEY=18253a0c54cd9a624a5dd541a6f7bb41dcc36c00
+
 # Load conda đúng path
 source /data/cndt_hangdv/miniconda3/bin/activate fontdiffuser || { echo "Failed to activate conda env"; exit 1; }
 
