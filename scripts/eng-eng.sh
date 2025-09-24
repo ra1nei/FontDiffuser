ddf8cab28c08aaf3e53c23e631d816b8505da278
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-09-24 22:13:36 +0700

    update

--- scripts/eng-eng.sh
+++ scripts/eng-eng.sh
@@ -1,9 +1,9 @@
 python sample.py \
     --ckpt_dir="./global_step_440000/" \
-    --content_image_path="./thesis-data-png/train/ContentImage/え.png" \
-    --style_image_path="./thesis-data-png/train/TargetImage/851CHIKARA-DZUYOKU-kanaB-2_chinese/851CHIKARA-DZUYOKU-kanaB-2_chinese+万.png" \
+    --content_image_path="./thesis-data-png/train/ContentImage/A.png" \
+    --style_image_path="./thesis-data-png/train/TargetImage/AaChaoMianJin-2_english/AaChaoMianJin-2_english+L+.png" \
     --save_image \
-    --save_image_dir="./hans-hans/example1" \
+    --save_image_dir="./eng-eng/example1" \
     --device="cuda:0" \
     --algorithm_type="dpmsolver++" \
     --guidance_type="classifier-free" \
@@ -13,10 +13,10 @@ python sample.py \
 
 python sample.py \
     --ckpt_dir="./global_step_440000/" \
-    --content_image_path="./thesis-data-png/train/ContentImage/え.png" \
-    --style_image_path="./thesis-data-png/train/TargetImage/A-OTF-CinemaLetterStd-Light-2_chinese/A-OTF-CinemaLetterStd-Light-2_chinese+事.png" \
+    --content_image_path="./thesis-data-png/train/ContentImage/A.png" \
+    --style_image_path="./thesis-data-png/train/TargetImage/║║╥╟▓╩╡√╠σ╝≥_english/║║╥╟▓╩╡√╠σ╝≥_english+M+.png" \
     --save_image \
-    --save_image_dir="./hans-hans/example2" \
+    --save_image_dir="./eng-eng/example2" \
     --device="cuda:0" \
     --algorithm_type="dpmsolver++" \
     --guidance_type="classifier-free" \
@@ -26,10 +26,10 @@ python sample.py \
 
 python sample.py \
     --ckpt_dir="./global_step_440000/" \
-    --content_image_path="./thesis-data-png/train/ContentImage/え.png" \
-    --style_image_path="./thesis-data-png/train/TargetImage/Classic Zong yi ti Font_chinese/Classic Zong yi ti Font_chinese+专.png" \
+    --content_image_path="./thesis-data-png/train/ContentImage/A.png" \
+    --style_image_path="./thesis-data-png/train/TargetImage/851CHIKARA-DZUYOKU-kanaB-2_english/851CHIKARA-DZUYOKU-kanaB-2_english+b.png" \
     --save_image \
-    --save_image_dir="./hans-hans/example3" \
+    --save_image_dir="./eng-eng/example3" \
     --device="cuda:0" \
     --algorithm_type="dpmsolver++" \
     --guidance_type="classifier-free" \
