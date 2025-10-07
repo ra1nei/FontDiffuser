da9d559d50f9fc8e56708a08696517f8f50b9427
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-07 22:46:01 +0700

    test new save_image_with_content_style

--- utils.py
+++ utils.py
@@ -25,21 +25,21 @@ def save_single_image(save_dir, image):
     image.save(save_path)
 
 
-def save_image_with_content_style(save_dir, image, content_image_pil, content_image_path, style_image_path, resolution, filename):
-    
-    new_image = Image.new('RGB', (resolution*3, resolution))
+
+def save_image_with_content_style(save_dir, image, content_image_pil, content_image_path, style_image_path, resolution, filename="out_with_cs.jpg"):
+    os.makedirs(save_dir, exist_ok=True)
+    new_image = Image.new('RGB', (resolution * 3, resolution))
     if content_image_pil is not None:
         content_image = content_image_pil
     else:
         content_image = Image.open(content_image_path).convert("RGB").resize((resolution, resolution), Image.BILINEAR)
     style_image = Image.open(style_image_path).convert("RGB").resize((resolution, resolution), Image.BILINEAR)
-
     new_image.paste(content_image, (0, 0))
     new_image.paste(style_image, (resolution, 0))
-    new_image.paste(image, (resolution*2, 0))
-
-    save_path = f"{save_dir}/out_with_cs.jpg"
+    new_image.paste(image, (resolution * 2, 0))
+    save_path = os.path.join(save_dir, filename)
     new_image.save(save_path)
+    return save_path
 
 
 def x0_from_epsilon(scheduler, noise_pred, x_t, timesteps):
