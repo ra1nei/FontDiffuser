f72969ceb8e97b3eb24551c6cdefe0ca3bc7548b
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-11-30 10:26:46 +0700

    Update: Chạy song song và nohup

--- scripts/p1_same_rsi.sh
+++ scripts/p1_same_rsi.sh
@@ -1,4 +1,11 @@
-git pull && accelerate launch --num_processes=2 train.py \
+export CUDA_VISIBLE_DEVICES=3,4
+
+LOGFILE="p1_same.log"
+
+echo "Starting training on GPU 3 and 4..."
+echo "Log file: $LOGFILE"
+
+git pull && nohup accelerate launch --num_processes=2 train.py \
     --seed=123 \
     --data_root="/datastore/cndt_hangdv/TDKD/FontDiffuser/image_dataset/FontDiffuser" \
     --report_to="wandb" \
@@ -23,5 +30,6 @@ git pull && accelerate launch --num_processes=2 train.py \
     --mixed_precision="no" \
     --lang_mode="same" \
     --experience_name="P1_SAME" \
-    --output_dir="ckpt/p1_same_64" \
-    --rsi_mode "rsi"
+    --output_dir="ckpt/p1_same" \
+    --rsi_mode "rsi" \
+    > "$LOGFILE" 2>&1 &
